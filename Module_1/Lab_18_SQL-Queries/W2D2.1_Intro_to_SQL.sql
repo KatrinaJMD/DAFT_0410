1b24e28ffe7e2b18ee458a14af584c1967b41e13
Katrina JMD <personal.k.konz@gmail.com>
2023-04-18 13:05:12 +0200

    updating files

---
+++ Module_1/Lab_18_SQL-Queries/W2D2.1_Intro_to_SQL.sql
@@ -0,0 +1,20 @@
+# Topics
+
+# Introducing:
+# SELECT, SELECT DISTINCT, LIMIT
+# Aggregations: COUNT, SUM, MIN, MAX
+# GROUP BY
+# ORDER BY
+# filter: WHERE
+# CASE (in select)
+# CASE (in where)
+# CREATE DATABASE
+# CREATE SCHEMA
+# USE
+
+
+# First, some basics....
+select 1*2;
+
+
+# After loading up the sakila database, lets make sure we are using it.
